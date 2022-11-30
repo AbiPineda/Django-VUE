b52ed109fa5fdaad71c6befad3305974d4604084
AbiPineda <pineverdi@gmail.com>
2022-11-30 12:13:59 -0600

    creando tabla para listado de libros

--- projectfacilito/frontend/src/components/Book/ListBook.vue
+++ projectfacilito/frontend/src/components/Book/ListBook.vue
@@ -1,21 +1,30 @@
 <template>
-<div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col text-left">
-            <h2>Listado de libros</h2>
+      <div class="col text-left">
+        <h2>Listado de libros</h2>
+
+        <div class="col-md-12">
+          <b-table striped hover :items="books" :fields="fields"></b-table>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
-
 <script>
-export default{
-
-}
+export default {
+  data() {
+    return {
+      fields: [
+        { key: "title", label: "Titulo" },
+        { key: "description", label: "Descripcion" },
+        { key: "action", label: "Accion" }
+      ],
+      books: []
+    };
+  }
+};
 </script>
 
-
-<style>
-
-</style>
+<style></style>
